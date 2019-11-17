6e85bcd69e94c899166422aa6c63b1d3d228f8c7
Nikane Mallea <nikane@mac.com>
2019-11-17 16:41:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_dogs.push()
+  my_dogs <<""
 end
 
 def add_element_to_start_of_array(array, element)
-  dogs.unshift("")
+  my_dogs.unshift("")
 end
 
 def remove_element_from_end_of_array(array)
